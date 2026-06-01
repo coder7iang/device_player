57633afdb6b44bbd08724af4171efd989b318e96
strong <91aqq@sina.com>
2026-06-01 10:16:39 +0800

    feat: 更新设备模型以支持 mDNS 服务名识别无线连接设备，修正应用名称为 DevicePlayer.app

--- lib/dialog/devices_model.dart
+++ lib/dialog/devices_model.dart
@@ -8,6 +8,9 @@ class DevicesModel extends ListFilterItem {
   DevicesModel(this.brand, this.model, this.id)
       : super(brand + " " + model + " " + id);
 
-  /// 是否为无线连接设备（ID 格式为 ip:port）
-  bool get isWireless => RegExp(r'^\d+\.\d+\.\d+\.\d+:\d+$').hasMatch(id);
+  /// 是否为无线连接设备（ip:port 或 mDNS 服务名）
+  bool get isWireless =>
+      RegExp(r'^\d+\.\d+\.\d+\.\d+:\d+$').hasMatch(id) ||
+      id.contains('_adb-tls-connect._tcp') ||
+      id.contains('_adb-tls-pairing._tcp');
 }
